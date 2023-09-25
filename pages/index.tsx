cb9fa04b0d160e3a6fe1075ee999975bde6b492a
Hemant Saraf <hemant.saraf@biz4solutions.com>
2023-09-25 11:46:28 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,17 @@ import { NextPageWithLayout } from "./_app";
 import dynamic from "next/dynamic";
 import HeroSection from "@/src/components/HeroSection";
 
-const AboutSection = dynamic(()=>import("@/src/components/AboutSection"))
+const AboutSection = dynamic(() => import("@/src/components/AboutSection"));
+const ProjectsSection = dynamic(
+  () => import("@/src/components/ProjectsSection")
+);
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
       <HeroSection />
-      <AboutSection/>
+      <AboutSection />
+      <ProjectsSection/>
     </>
   );
 };
